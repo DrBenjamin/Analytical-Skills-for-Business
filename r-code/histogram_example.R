2382d86c1317228119f535ffa497f94257c23151
DrBenjamin <ben@seriousbenentertainment.org>
2025-09-02 12:04:13 +0200

    added note from bib

--- r-code/histogram_example.R
+++ r-code/histogram_example.R
@@ -4,17 +4,18 @@
 # Loading the built-in mtcars dataset
 data(mtcars)
 
-# Creating a simple histogram of miles per gallon (mpg)
-hist(mtcars$mpg, 
+# Creating a simple histogram of miles per gallon
+hist(mtcars$mpg,
      main = "Histogram of Miles Per Gallon",
-     xlab = "Miles Per Gallon (mpg)",
+     xlab = "Miles Per Gallon",
      ylab = "Frequency",
      col = "lightblue",
-     border = "black",
-     breaks = 10)
+     border = "black"
+)
 
 # Adding some basic statistics to the plot
 abline(v = mean(mtcars$mpg), col = "red", lwd = 2, lty = 2)
-legend("topright", legend = paste("Mean =", round(mean(mtcars$mpg), 2)), 
-       col = "red", lty = 2, lwd = 2)
-
+legend("topright",
+     legend = paste("Mean =", round(mean(mtcars$mpg), 2)),
+     col = "red", lty = 2, lwd = 2
+)
